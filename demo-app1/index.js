2db58ae7766c7fb684f2d0296793b57e36637c33
mukulsharma0917 <mukulsharma0917@gmail.com>
2022-03-07 17:35:07 +0530

    test
    test

--- demo-app1/index.js
+++ demo-app1/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const mySuccessFunction = function () {
+    console.log('Success, You are a GEEK');
+}
+
+const myPromise = new Promise(function(resolve, reject) {
+  });
+
+myPromise.then(mySuccessFunction).catch(function () {
+    console.log('Some error has occurred');
+});
+
